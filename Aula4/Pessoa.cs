bfcd0077ad1c1f88414e9d69110abe0a5b387b15
Ygor Canalli <ygor.canalli@gmail.com>
2021-03-03 12:54:26 -0300

    classe pessoa

---
+++ Aula4/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Aula4
+{
+    public class Pessoa
+    {
+        public String Nome;
+        public DateTime DataDeNascimento;
+        public Int32 Altura;
+        public String CorDoCabelo;
+    }
+}
--- Aula4/Program.cs
+++ Aula4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Aula4
 {
@@ -7,6 +8,19 @@ namespace Aula4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Pessoa p = new Pessoa();
+            p.Nome = "Fulano da Silva";
+            p.Altura = 175;
+
+            CultureInfo idioma = new CultureInfo("pt-BR");
+            String formato = "dd/MM/yyyy";
+            p.DataDeNascimento = DateTime.ParseExact("29/02/2000", formato, idioma);
+            p.CorDoCabelo = "Castanho";
+            
+            Console.WriteLine("Nome: {0}", p.Nome);
+            Console.WriteLine("Altura: {0}", p.Altura);
+            Console.WriteLine("Data de Nascimento: {0}", p.DataDeNascimento);
+            Console.WriteLine("Cor do Cabelo: {0}", p.CorDoCabelo);
         }
     }
 }
