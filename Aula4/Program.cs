fd4396376347b196ce5e862c90886aa1de61ae25
Ygor Canalli <ygor.canalli@gmail.com>
2021-03-04 02:09:40 -0300

    criação de data

--- Aula4/Program.cs
+++ Aula4/Program.cs
@@ -8,19 +8,19 @@ namespace Aula4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Pessoa p = new Pessoa();
-            p.Nome = "Fulano da Silva";
-            p.Altura = 175;
-
-            CultureInfo idioma = new CultureInfo("pt-BR");
-            String formato = "dd/MM/yyyy";
-            p.DataDeNascimento = DateTime.ParseExact("29/02/2000", formato, idioma);
-            p.CorDoCabelo = "Castanho";
+            Pessoa minhaPessoa = new Pessoa();
             
-            Console.WriteLine("Nome: {0}", p.Nome);
-            Console.WriteLine("Altura: {0}", p.Altura);
-            Console.WriteLine("Data de Nascimento: {0}", p.DataDeNascimento);
-            Console.WriteLine("Cor do Cabelo: {0}", p.CorDoCabelo);
+            minhaPessoa.Nome = "Fulano da Silva";
+            minhaPessoa.Altura = 175;
+            minhaPessoa.CorDoCabelo = "Castanho";
+
+            Console.WriteLine("Nome: {0}", minhaPessoa.Nome);
+            Console.WriteLine("Altura: {0}", minhaPessoa.Altura);
+            Console.WriteLine("Cor do Cabelo: {0}", minhaPessoa.CorDoCabelo);
+            
+            DateTime minhaData = new DateTime(2000, 2, 29);
+            minhaPessoa.DataDeNascimento = minhaData;
+            Console.WriteLine("Data de Nascimento: {0}", minhaPessoa.DataDeNascimento);
         }
     }
 }
