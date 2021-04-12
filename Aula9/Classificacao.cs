3f8087c3c3c6eb11892e5cee6a028c952b1ee2f2
Ygor Canalli <ygor.canalli@gmail.com>
2021-04-12 15:17:21 -0300

    renomeando classificacao.cs

---
+++ Aula9/Classificacao.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Aula9
+{
+    public class Classificacao
+    {
+        public String Familia;
+        public String Genero;
+        public String Especie;
+
+        public Classificacao(String familia, String genero, String especie)
+        {
+            
+            
+            
+        }
+    }
+}
