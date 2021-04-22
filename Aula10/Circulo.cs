cb48ab370093c19ae7d24bce44f8fe4cb50640fd
Ygor Canalli <ygor.canalli@gmail.com>
2021-04-22 04:25:28 -0300

    Figura e Circulo

---
+++ Aula10/Circulo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Aula10
+{
+public class Circulo : Figura 
+{
+    private double _raio; // atributo
+    public double Raio // propriedade
+    {
+        // leitura do atributo correspondente
+        get
+        {
+            return this._raio;
+        }
+        
+        // escrita do atributo correspondente
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Raio inválido");
+            }
+            else
+            {
+                this._raio = value;
+                this.AtualizarArea();
+                this.AtualizarPerimetro();
+            }
+        }
+    }
+    public Circulo(double raio)
+    {
+        // escrita utilizando método set da propriedade
+        this.Raio = raio;
+    }
+    private void AtualizarArea()
+    {
+        this._area = Math.PI * this._raio * this._raio;
+    }
+    private void AtualizarPerimetro()
+    {
+        this._perimetro = 2 * Math.PI * this._raio;
+    }
+}
+}
---
+++ Aula10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Aula10
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        Circulo circ1 = new Circulo(2.0);
+        
+        // acessando métodos get
+        Console.WriteLine("Raio:\t\t{0:0.0}", circ1.Raio);
+        Console.WriteLine("Área:\t\t{0:0.0}", circ1.Area);
+        Console.WriteLine("Perímetro\t{0:0.0}", circ1.Perimetro);
+
+        // acessando método set
+        // atualização automática
+        // da área e perímetro
+        circ1.Raio = 3.0;
+        
+        // acessando métodos get
+        Console.WriteLine("Raio:\t\t{0:0.0}", circ1.Raio);
+        Console.WriteLine("Área:\t\t{0:0.0}", circ1.Area);
+        Console.WriteLine("Perímetro\t{0:0.0}", circ1.Perimetro);
+
+        // valor inválido no método set
+        circ1.Raio = -3.0;
+        
+        // acessando métodos get
+        Console.WriteLine("Raio:\t\t{0:0.0}", circ1.Raio);
+        Console.WriteLine("Área:\t\t{0:0.0}", circ1.Area);
+        Console.WriteLine("Perímetro\t{0:0.0}", circ1.Perimetro);
+    }
+}
+}
