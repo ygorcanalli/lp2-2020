142f145ce2d7bdcb0726e68d5dc2837f11fb8864
Ygor Canalli <ygor.canalli@gmail.com>
2021-07-08 15:59:26 -0300

    gabarito tarefa 2

---
+++ GabaritoTarefa2/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GabaritoTarefa2
+{
+    public class Pessoa
+    {
+        private string Nome { get; set; }
+        private DateTime DataDeNascimento { get; set; }
+        private int Altura { get; set; }
+        private string CorDoCabelo { get; set; }
+
+
+        public Pessoa(string nome, DateTime dataDeNascimento, int altura, string corDoCabelo)
+        {
+            this.Nome = nome;
+            this.DataDeNascimento = dataDeNascimento;
+            this.Altura = altura;
+            this.CorDoCabelo = corDoCabelo;
+        }
+
+        public void ImprimirPessoa()
+        {
+            Console.WriteLine("Nome:\t\t\t{0}", this.Nome);
+            Console.WriteLine("Data de Nascimento:\t{0}", this.DataDeNascimento);
+            Console.WriteLine("Altura:\t\t\t{0}", this.Altura);
+            Console.WriteLine("Cor do Cabelo:\t\t{0}", this.CorDoCabelo);
+        }
+    }
+}
---
+++ GabaritoTarefa2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GabaritoTarefa2
+{
+    class Program
+    {
+
+        static void Incluir(List<Pessoa> pessoas)
+        {
+            Console.WriteLine("\nIncluir Pessoa");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Data de nascimento: ");
+            DateTime dataNascimento = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Altura: ");
+            int altura = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Cor do cabelo: ");
+            string corDoCabelo = Console.ReadLine();
+
+            Pessoa p = new Pessoa(nome, dataNascimento, altura, corDoCabelo);
+            pessoas.Add(p);
+        }
+
+        static void Imprimir(List<Pessoa> pessoas)
+        {
+            Console.WriteLine("======Pessoas cadastradas======");
+            foreach (var p in pessoas)
+            {
+                Console.WriteLine("\n-----------------");
+                p.ImprimirPessoa();
+            }
+            Console.WriteLine("===============================");
+        }
+        static void Main(string[] args)
+        {
+            int opcao;
+            List<Pessoa> pessoas = new List<Pessoa>();
+            do
+            {
+                Console.WriteLine("Sistema de Cadastro de Pessoas - Opções disponíveis:");
+                Console.WriteLine("[1] Incluir");
+                Console.WriteLine("[2] Consultar");
+                Console.WriteLine("[0] Sair\n");
+                Console.Write("Opção: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Incluir(pessoas);
+                        break;
+                    case 2:
+                        Imprimir(pessoas);
+                        break;
+                    case 0:
+                        Console.WriteLine("Adeus!");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            } while (opcao != 0);
+        }
+    }
+}
